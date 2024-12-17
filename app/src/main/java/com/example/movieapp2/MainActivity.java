d2472b5b613f4bd99d60b23bf1a1316367f2ecbf
Adarsh Vinod <adarshvnd2@gmail.com>
2024-12-17 13:43:14 +0530

    project updated

--- app/src/main/java/com/example/movieapp2/MainActivity.java
+++ app/src/main/java/com/example/movieapp2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.movieapp2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText et1,et2;
+    Button b1;
+    String getNum1,getNum2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        et1=(EditText) findViewById(R.id.uname);
+        et2=(EditText) findViewById(R.id.pass);
+        b1=(Button) findViewById(R.id.login);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getNum1=et1.getText().toString();
+                getNum2=et2.getText().toString();
+                if (getNum1.equals("admin") && getNum2.equals("12345")) {
+                    Intent i = new Intent(getApplicationContext(), MenuActivity2.class);
+                    startActivity(i);
+                }
+            }
         });
+
+
+
     }
 }
